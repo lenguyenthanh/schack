5daa2478e5b041b8f5d8a9b7de66c7cb0312aa1d
Thanh Le <lenguyenthanh@hotmail.com>
2022-05-21 22:49:13 +0200

    Correct source folder

---
+++ core/shared/src/test/scala/se/thanh/chess/bitboard/BitBoardTests.scala
@@ -0,0 +1,85 @@
+package se.thanh.chess.bitboard
+
+import se.thanh.chess.core.Arbitraries.given
+import se.thanh.chess.core.{ Color, Square }
+
+import munit.ScalaCheckSuite
+import org.lichess.compression.game.Bitboard as CBB
+import org.scalacheck.Prop
+
+import Bitboard.*
+
+class BitboardTests extends ScalaCheckSuite:
+
+  val allDeltas =
+    List(KING_DELTAS, KNIGHT_DELTAS, BISHOP_DELTAS, ROOK_DELTAS, KING_DELTAS, WHITE_PAWN_DELTAS, BLACK_PAWN_DELTAS)
+
+  def as(array: Array[Int]) = array.map(i => s"$i, ").mkString
+
+  test("single cases") {
+    val occupied = 20292374
+    val deltas   = KING_DELTAS
+    val s        = Square(43).get
+    val result   = Bitboard.slidingAttacks(s, occupied, deltas)
+    val expected = CBB.slidingAttacks(s, occupied, deltas)
+    assertEquals(result, expected)
+  }
+
+  test("slidingAttack with all occupied") {
+    val occupied = Bitboard.ALL
+    for
+      i      <- 0 to 63
+      deltas <- allDeltas
+      s        = Square(i).get
+      result   = Bitboard.slidingAttacks(s, occupied, deltas)
+      expected = CBB.slidingAttacks(s, occupied, deltas)
+    yield assertEquals(result, expected)
+  }
+
+  property("slidingAttack check") {
+    Prop.forAll { (occupied: Long, s: Square) =>
+      val result = for
+        deltas <- allDeltas
+        // _        = println(s"o: $occupied, s: $s, d: ${as(deltas)}")
+        result   = Bitboard.slidingAttacks(s, occupied, deltas)
+        expected = CBB.slidingAttacks(s, occupied, deltas)
+      yield result == expected
+      result.forall(identity)
+    }
+  }
+
+  test("init function") {
+    assertEquals(Bitboard.KNIGHT_ATTACKS.toSeq, CBB.KNIGHT_ATTACKS.toSeq)
+    assertEquals(Bitboard.WHITE_PAWN_ATTACKS.toSeq, CBB.WHITE_PAWN_ATTACKS.toSeq)
+    assertEquals(Bitboard.BLACK_PAWN_ATTACKS.toSeq, CBB.BLACK_PAWN_ATTACKS.toSeq)
+    assertEquals(Bitboard.ATTACKS.toSeq, CBB.ATTACKS.toSeq)
+    (0 until 64).foreach { i =>
+      assertEquals(Bitboard.RAYS(i).toSeq, CBB.RAYS(i).toSeq)
+      assertEquals(Bitboard.BETWEEN(i).toSeq, CBB.BETWEEN(i).toSeq)
+    }
+  }
+
+  property("bitshop attacks") {
+    Prop.forAll { (occupied: Long, s: Square) =>
+      s.bishopAttacks(occupied) == CBB.bishopAttacks(s, occupied)
+    }
+  }
+
+  property("rook attacks") {
+    Prop.forAll { (occupied: Long, s: Square) =>
+      s.rookAttacks(occupied) == CBB.rookAttacks(s, occupied)
+    }
+  }
+
+  property("queen attacks") {
+    Prop.forAll { (occupied: Long, s: Square) =>
+      s.queenAttacks(occupied) == CBB.queenAttacks(s, occupied)
+    }
+  }
+
+  property("pawn attacks") {
+    Prop.forAll { (s: Square) =>
+      s.pawnAttacks(Color.White) == CBB.pawnAttacks(true, s)
+      s.pawnAttacks(Color.Black) == CBB.pawnAttacks(false, s)
+    }
+  }
---
+++ core/shared/src/test/scala/se/thanh/chess/bitboard/StandardMoveGeneratorTests.scala
@@ -0,0 +1,145 @@
+package se.thanh.chess.bitboard
+
+import se.thanh.chess.core.*
+
+import munit.FunSuite
+import org.lichess.compression.game.MoveList
+
+import StandardMovesGenerator.*
+import Helpers.*
+
+class StandardMovesGeneratorTests extends FunSuite:
+
+  test("genEnPassant 1") {
+    val b        = Board.empty.copy(pawns = 0x7000000000L, white = 0x5000000000L, black = 0x2000000000L)
+    val ep       = Square.square(File.f, Rank.sixth)
+    val fen      = Fen(b, State(Color.White, Some(ep), Bitboard.corners, 5, 10))
+    val moves    = fen.genEnPassant(ep)
+    val expected = Set(Move.EnPassant(Square(36).get, ep), Move.EnPassant(Square(38).get, ep))
+    assertEquals(moves.toSet, expected.toSet)
+  }
+
+  test("genPawn for standard position") {
+    val fen           = Fen.standard
+    val targets       = ~fen.us
+    val moves         = fen.genPawn(targets)
+    val moveList      = MoveList()
+    val expectedMoves = fen.cBoard.genPawn(targets, moveList)
+    assertMoves(moves, moveList)
+  }
+
+  test("genPawn with fenFixtures") {
+    FenFixtures.fens.foreach { str =>
+      val fen           = Fen.parse(str).getOrElse(throw RuntimeException("boooo"))
+      val targets       = ~fen.us
+      val moves         = fen.genPawn(targets)
+      val moveList      = MoveList()
+      val expectedMoves = fen.cBoard.genPawn(targets, moveList)
+      assertMoves(moves, moveList)
+    }
+  }
+
+  test("genKnight with fenFixtures") {
+    FenFixtures.fens.foreach { str =>
+      val fen           = Fen.parse(str).getOrElse(throw RuntimeException("boooo"))
+      val targets       = ~fen.us
+      val moves         = fen.genKnight(targets)
+      val moveList      = MoveList()
+      val expectedMoves = fen.cBoard.genKnight(targets, moveList)
+      assertMoves(moves, moveList)
+    }
+  }
+
+  test("genBishop with fenFixtures") {
+    FenFixtures.fens.foreach { str =>
+      val fen           = Fen.parse(str).getOrElse(throw RuntimeException("boooo"))
+      val targets       = ~fen.us
+      val moves         = fen.genBishop(targets)
+      val moveList      = MoveList()
+      val expectedMoves = fen.cBoard.genBishop(targets, moveList)
+      assertMoves(moves, moveList)
+    }
+  }
+
+  test("genRook with fenFixtures") {
+    FenFixtures.fens.foreach { str =>
+      val fen           = Fen.parse(str).getOrElse(throw RuntimeException("boooo"))
+      val targets       = ~fen.us
+      val moves         = fen.genRook(targets)
+      val moveList      = MoveList()
+      val expectedMoves = fen.cBoard.genRook(targets, moveList)
+      assertMoves(moves, moveList)
+    }
+  }
+
+  test("genQueen with fenFixtures") {
+    FenFixtures.fens.foreach { str =>
+      val fen           = Fen.parse(str).getOrElse(throw RuntimeException("boooo"))
+      val targets       = ~fen.us
+      val moves         = fen.genQueen(targets)
+      val moveList      = MoveList()
+      val expectedMoves = fen.cBoard.genQueens(targets, moveList)
+      assertMoves(moves, moveList)
+    }
+  }
+
+  test("genNonKing with fenFixtures") {
+    FenFixtures.fens.foreach { str =>
+      val fen           = Fen.parse(str).getOrElse(throw RuntimeException("boooo"))
+      val targets       = ~fen.us
+      val moves         = fen.genNonKing(targets)
+      val moveList      = MoveList()
+      val expectedMoves = fen.cBoard.genNonKing(targets, moveList)
+      assertMoves(moves, moveList)
+    }
+  }
+
+  test("genSafeKing with fenFixtures") {
+    FenFixtures.fens.foreach { str =>
+      val fen           = Fen.parse(str).getOrElse(throw RuntimeException("boooo"))
+      val king          = fen.ourKing.get
+      val targets       = ~fen.us
+      val moves         = fen.genSafeKing(king, targets)
+      val moveList      = MoveList()
+      val expectedMoves = fen.cBoard.genSafeKing(king, targets, moveList)
+      assertMoves(moves, moveList)
+    }
+  }
+
+  test("genCastling with fenFixtures") {
+    FenFixtures.fens.foreach { str =>
+      val fen           = Fen.parse(str).getOrElse(throw RuntimeException("boooo"))
+      val king          = fen.ourKing.get
+      val targets       = ~fen.us
+      val moves         = fen.genCastling(king)
+      val moveList      = MoveList()
+      val expectedMoves = fen.cBoard.genCastling(king, moveList)
+      assertMoves(moves, moveList)
+    }
+  }
+
+  test("genEvasion with fenFixtures") {
+    FenFixtures.fens.foreach { str =>
+      val fen           = Fen.parse(str).getOrElse(throw RuntimeException("boooo"))
+      val king          = fen.ourKing.get
+      val checkers      = fen.checkers.get
+      val moves         = fen.genEvasions(king, checkers)
+      val moveList      = MoveList()
+      val expectedMoves = fen.cBoard.genEvasions(king, checkers, moveList)
+      assertMoves(moves, moveList)
+    }
+  }
+
+  test("legalMoves with fenFixtures") {
+    FenFixtures.fens.foreach { str =>
+      val fen           = Fen.parse(str).getOrElse(throw RuntimeException("boooo"))
+      val moves         = fen.generate
+      val moveList      = MoveList()
+      val expectedMoves = fen.cBoard.legalMoves(moveList)
+      assertMoves(moves, moveList)
+    }
+  }
+
+  private def assertMoves(moves: List[Move], moveList: MoveList) =
+    assertEquals(moves.length, moveList.size)
+    assertEquals(moves.map(_.uci).toSet, moveList.uciSet)
